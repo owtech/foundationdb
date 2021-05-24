bf2a9f31d52db68be1102b4b6146f1c718a41292
Oleg Samarin <osamarin@openwaygroup.com>
2021-05-24 13:39:33 +0300

    Fixed compilation with a recent compiler

--- cmake/CompileRocksDB.cmake
+++ cmake/CompileRocksDB.cmake
@@ -51,6 +51,7 @@ else()
                -DWITH_ZSTD=OFF
                -DROCKSDB_BUILD_SHARED=OFF
                -DCMAKE_POSITION_INDEPENDENT_CODE=True
+               -DFAIL_ON_WARNINGS=${USE_WERROR}
     BUILD_BYPRODUCTS <BINARY_DIR>/librocksdb.a
     INSTALL_COMMAND ""
   )
