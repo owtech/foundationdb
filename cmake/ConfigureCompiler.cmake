98ce6894d5962e5837f5599a5800e1b66fcdad53
Oleg Samarin <osamarin@openwaygroup.com>
2021-07-05 20:25:25 +0300

    Fixed build with gcc <8

--- cmake/ConfigureCompiler.cmake
+++ cmake/ConfigureCompiler.cmake
@@ -330,9 +330,9 @@ else()
     -fvisibility=hidden
     -Wreturn-type
     -fPIC)
-  if (CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "^x86")
-    add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-Wclass-memaccess>)
-  endif()
+#  if (CMAKE_HOST_SYSTEM_PROCESSOR MATCHES "^x86")
+#    add_compile_options($<$<COMPILE_LANGUAGE:CXX>:-Wclass-memaccess>)
+#  endif()
   if (GPERFTOOLS_FOUND AND GCC)
     add_compile_options(
       -fno-builtin-malloc
