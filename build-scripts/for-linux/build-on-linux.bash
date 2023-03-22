847b2640f311c39c349ef8b5f7c275918e12f2ee
Oleg Samarin <osamarin@openintegration.inc>
2023-03-22 12:03:49 +0300

    Switched build to using build scripts

---
+++ build-scripts/for-linux/build-on-linux.bash
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# $1 - Version
+# $2 - Build version
+# $3 - Release flag
+# $4 - Paralllel threads
+# $5 - Source Dir. If not set then relative to the script dir
+
+set -e
+
+BASE_DIR="$(readlink -f $(dirname $0))"
+source $BASE_DIR/../set-ver-prms.sh "$1" "$2"
+RELEASE_FLAG=${3:-OFF}
+PARALLEL_PRMS="-j ${4:-$(nproc)}"
+SRC_DIR=${5:-$(readlink -f $BASE_DIR/../..)}
+
+START_DIR=`pwd`
+BUILD_DIR=$START_DIR/bld/linux
+
+mkdir -p $BUILD_DIR
+pushd $BUILD_DIR
+
+rm -rf *
+export LANG=C
+
+APP_PRMS="\
+  $CMAKE_VERSION_PRMS \
+  -DFDB_RELEASE=$RELEASE_FLAG \
+  -DGENERATE_DEBUG_PACKAGES=OFF \
+  -DUSE_LIBCXX=OFF \
+  -DSTATIC_LINK_LIBCXX=ON \
+  -DENABLE_SIMULATION_TESTS=ON"
+
+[ ! -e /usr/lib64/libcrypto.a -a -e /opt/openssl/lib/libcrypto.a ] && \
+  APP_PRMS="$APP_PRMS -DOPENSSL_ROOT_DIR=/opt/openssl"
+
+# find libatomic
+source $BASE_DIR/build-find-libatomic.bash
+
+echo "env CC=clang CXX=clang++ cmake -G Ninja $APP_PRMS . $SRC_DIR"
+env CC=clang CXX=clang++ cmake -G Ninja $APP_PRMS . $SRC_DIR
+
+ninja $PARALLEL_PRMS -k 0
+
+cpack -G RPM
+for fn in packages/*.rpm; do echo "$fn:"; rpm -qpRv $fn; echo; done
+
+cpack -G DEB
+for fn in packages/*.deb; do echo "$fn:"; dpkg -I $fn; done
+
+ninja $PARALLEL_PRMS -k 0 documentation/package_html
+
+# make all-binaries archive
+# calculate filename
+BINS_FILENAME=`ls -1 packages/foundationdb-docs-* | sed s/-docs/-bins/`
+tar -I pigz -cvf $BINS_FILENAME bin
+
+popd
+
