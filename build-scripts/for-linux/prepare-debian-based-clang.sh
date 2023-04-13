abcc95b8291daa04be8c2ddbdc5d0824b9f2a13a
Oleg Samarin <osamarin68@gmail.com>
2023-04-13 12:50:40 +0300

    Ow fork main build debian10 (#69)
    * Added capability of building on debian 10
    
    (cherry picked from commit 82c37403643e6b71f48bb746b1817c7549520fc2)
    
    * Fixed error in a script
    
    (cherry picked from commit a04986767de573be76036204a2f808e99182691f)
    
    * Removed extra script
    
    (cherry picked from commit 9454b5fb1521478568a77bd1706157fc61fb2342)
    
    * Switched the base build image to debian:10
    
    ---------
    
    Co-authored-by: Oleg Samarin <osamarin@openintegration.inc>

--- build-scripts/for-linux/prepare-debian-based-clang.sh
+++ build-scripts/for-linux/prepare-debian-based-clang.sh
@@ -5,9 +5,12 @@
 set -e
 
 sudo apt update
-RECENT_PKG=`apt search -o APT::Cache::Search::Version=1 '^clang-[0-9][0-9]$' | awk '{print $1;}' | sort | tail -n 1`
+RECENT_VERSION=` \
+  apt search -o APT::Cache::Search::Version=1 '^clang-[0-9]+$' \
+    | tr - ' ' | awk '{print $2;}' | sort -n  | tail -n 1`
 
-sudo DEBIAN_FRONTEND=noninteractive apt install -y $RECENT_PKG
+sudo DEBIAN_FRONTEND=noninteractive apt install -y \
+  clang-$RECENT_VERSION libc++-$RECENT_VERSION-dev libc++abi-$RECENT_VERSION-dev 
 
 [ -e /usr/local/bin/clang ] || sudo ln -s `ls -1 /usr/bin/clang-[0-9]* | tail -n 1` /usr/local/bin/clang
 [ -e /usr/local/bin/clang++ ] || sudo ln -s `ls -1 /usr/bin/clang++-[0-9]* | tail -n 1` /usr/local/bin/clang++
