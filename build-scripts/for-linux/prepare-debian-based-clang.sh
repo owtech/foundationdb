544fae43613adb720b1834aa46ab922d7f175531
Oleg Samarin <osamarin@openintegration.inc>
2023-04-13 11:03:19 +0300

    Fixed error in a script
    (cherry picked from commit a04986767de573be76036204a2f808e99182691f)

--- build-scripts/for-linux/prepare-debian-based-clang.sh
+++ build-scripts/for-linux/prepare-debian-based-clang.sh
@@ -10,7 +10,7 @@ RECENT_VERSION=` \
     | tr - ' ' | awk '{print $2;}' | sort -n  | tail -n 1`
 
 sudo DEBIAN_FRONTEND=noninteractive apt install -y \
-  $clang-$RECENT_VERSION libc++-$RECENT_VERSION-dev libc++abi-$RECENT_VERSION-dev 
+  clang-$RECENT_VERSION libc++-$RECENT_VERSION-dev libc++abi-$RECENT_VERSION-dev 
 
 [ -e /usr/local/bin/clang ] || sudo ln -s `ls -1 /usr/bin/clang-[0-9]* | tail -n 1` /usr/local/bin/clang
 [ -e /usr/local/bin/clang++ ] || sudo ln -s `ls -1 /usr/bin/clang++-[0-9]* | tail -n 1` /usr/local/bin/clang++
