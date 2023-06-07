af365618b85b60e39bee303fe9c31ee3dacd32fb
Oleg Samarin <osamarin@openintegration.inc>
2023-06-07 13:36:42 +0300

    Fixed checking for swift

--- build-scripts/for-linux/find-swift.bash
+++ build-scripts/for-linux/find-swift.bash
@@ -3,4 +3,12 @@
 # find swift installation
 # print the path to swift installed or return non zero rc
 
-which swiftc || ls -1d /opt/swift*/usr/bin/swiftc 2>/dev/null | tail -n 1
+
+if ! which swiftc 2>/dev/null; then
+  WILDCARDS="/opt/swift*/usr/bin/swiftc"
+  if ls $WILDCARDS 2>/dev/null; then
+    ls -1d $WILDCARDS | tail -n 1
+  else  
+    return $? 2>/dev/null || exit $?
+  fi
+fi
