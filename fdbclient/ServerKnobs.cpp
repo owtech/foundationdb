de6d6767fcf2fedf956e20e5d869b4cf9b0899bc
Oleg Samarin <osamarin@openintegration.inc>
2023-11-14 15:16:56 +0300

    Added a new batching algorithm
    (cherry picked from commit 6313478266b41fed9c966d81bece1092c80e03ff)
    
    # Conflicts:
    #	fdbclient/ServerKnobs.cpp
    #	fdbclient/include/fdbclient/ServerKnobs.h
    #	fdbserver/CommitProxyServer.actor.cpp

--- fdbclient/ServerKnobs.cpp
+++ fdbclient/ServerKnobs.cpp
@@ -575,6 +575,7 @@ void ServerKnobs::initialize(Randomize randomize, ClientKnobs* clientKnobs, IsSi
 	init( COMMIT_PROXY_LIVENESS_TIMEOUT,                        20.0 );
 
 	init( COMMIT_BATCH_RANDOMIZE_INTERVAL,                      false );
+	init( COMMIT_BATCH_MAX_IN_PROGRESS,                             0 );
 	init( COMMIT_TRANSACTION_BATCH_INTERVAL_FROM_IDLE,         0.0005 ); if( randomize && BUGGIFY ) COMMIT_TRANSACTION_BATCH_INTERVAL_FROM_IDLE = 0.005;
 	init( COMMIT_TRANSACTION_BATCH_INTERVAL_MIN,                0.001 ); if( randomize && BUGGIFY ) COMMIT_TRANSACTION_BATCH_INTERVAL_MIN = 0.1;
 	init( COMMIT_TRANSACTION_BATCH_INTERVAL_MAX,                0.020 );
